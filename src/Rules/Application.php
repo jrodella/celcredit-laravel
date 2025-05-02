132aacb120b937410942fb5f07aad110b467ca34
Jonathan Delgado <jonathan@pixcred.io>
2025-05-02 14:29:20 -0300

    new field: beneficiary_account.registered_account_id

--- src/Rules/Application.php
+++ src/Rules/Application.php
@@ -12,6 +12,7 @@ class Application
             'product.id' => 'required|uuid',
             'borrower.id' => 'required|uuid',
             'funding.id' => 'required|uuid',
+            'beneficiary_account.registered_account_id' => 'required|uuid',
             'payment_method' => ['required', Rule::in(['PIX'])],
             'signature_collect_method' => ['required', Rule::in(['LINK', 'NONE', 'SMS', 'EMAIL', 'PHYSICAL'])],
             'num_payments' => 'required|integer|min:1',
